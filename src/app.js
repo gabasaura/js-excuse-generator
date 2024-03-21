7524bb0b5aebf285ada345d0e70657d9be1055b3
Gabriela Garin <gabriela.garin@gmail.com>
2024-03-21 05:09:39 +0000

    generador de excusas avance

--- src/app.js
+++ src/app.js
@@ -7,5 +7,38 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  const laExcusa = document.getElementById("excuse");
+
+  let who = [
+    "The cat",
+    "My grandma",
+    "The mailman",
+    "My bird",
+    "My sister",
+    "My hamster"
+  ];
+  let action = ["ate", "peed", "crushed", "broke", "stole", "burn"];
+  let what = [
+    "my homework",
+    "my phone",
+    "the car",
+    "my bicycle",
+    "my notebook"
+  ];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomAction = action[Math.floor(Math.random() * action.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+  // Display the excuse
+  laExcusa.textContent = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
 };
